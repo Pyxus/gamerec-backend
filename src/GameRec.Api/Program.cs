6b78745cfb13963abdab82cadb4185c96107e40d
pyxus <eze.halley@gmail.com>
2023-11-30 08:15:26 -0500

    Add IGDBClient

--- src/GameRec.Api/Program.cs
+++ src/GameRec.Api/Program.cs
@@ -1,15 +1,30 @@
+using GameRec.Api.Repositories;
+
 GameRec.Api.Util.DotEnv.Load();
+
+#region Testing
+var clientId = Environment.GetEnvironmentVariable("TWITCH_CLIENT_ID");
+var clientSecret = Environment.GetEnvironmentVariable("TWITCH_CLIENT_SECRET");
+
+if (clientId != null && clientSecret != null)
+{
+    var igdbClient = new IGDBClient(clientId, clientSecret);
+
+    await igdbClient.RefreshAuth();
+}
+
+#endregion
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddHealthChecks();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 app.MapHealthChecks("/health");
 
---
+++ src/GameRec.Api/Repositories/IGDBClient.cs
@@ -0,0 +1,61 @@
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GameRec.Api.Repositories
+{
+    public class IGDBClient
+    {
+        private Auth? _auth;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly HttpClient _httpClient = new();
+
+        public IGDBClient(string twitchId, string twitchSecret)
+        {
+            _clientId = twitchId;
+            _clientSecret = twitchSecret;
+        }
+
+        public async Task RefreshAuth()
+        {
+            var endpointParams = $"client_id={_clientId}&client_secret={_clientSecret}&grant_type=client_credentials";
+            var endpoint = $"https://id.twitch.tv/oauth2/token?{endpointParams}";
+            var response = await _httpClient.PostAsync(endpoint, null);
+
+            try
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (content != null)
+                {
+                    _auth = JsonSerializer.Deserialize<Auth>(content);
+                    Console.WriteLine(_auth?.AccessToken);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+        private struct Auth
+        {
+            [JsonPropertyName("access_token")]
+            public string AccessToken { get; set; } = "";
+            [JsonPropertyName("expires_in")]
+            public int ExpiresIn { get; set; } = 0;
+            [JsonPropertyName("token_type")]
+            public string TokenType { get; set; } = "";
+            [JsonIgnore]
+            public DateTime RefreshedAt { get; set; }
+
+            public Auth()
+            {
+                RefreshedAt = DateTime.Now;
+            }
+
+        }
+    }
+}
