e1845c238b9482fef50f6a0bd9d6dbc198718ebe
pyxus <eze.halley@gmail.com>
2023-12-08 01:58:59 -0500

    Refresh auth before adding IgdbClient as singleton

--- src/GameRec.Api/Program.cs
+++ src/GameRec.Api/Program.cs
@@ -16,7 +16,10 @@ builder.Services.AddSingleton(provider =>
     var httpClientFactory = provider.GetRequiredService<IHttpClientFactory>();
     var clientId = Environment.GetEnvironmentVariable("TWITCH_CLIENT_ID");
     var clientSecret = Environment.GetEnvironmentVariable("TWITCH_CLIENT_SECRET");
-    return new IgdbClient(httpClientFactory, clientId!, clientSecret!);
+    var igdbClient = new IgdbClient(httpClientFactory, clientId!, clientSecret!);
+    igdbClient.RefreshAuth().Wait();
+
+    return igdbClient;
 });
 
 var app = builder.Build();
