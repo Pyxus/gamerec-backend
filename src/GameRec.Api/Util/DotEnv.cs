2d3a23c82daf231890baddacaa070cf00a2af478
pyxus <eze.halley@gmail.com>
2023-12-24 23:15:00 -0500

    Add doc comments to DotEnv class

--- src/GameRec.Api/Util/DotEnv.cs
+++ src/GameRec.Api/Util/DotEnv.cs
@@ -1,27 +1,30 @@
 
-namespace GameRec.Api.Util
+namespace GameRec.Api.Util;
+
+/// <summary>
+/// Simple utility class for loading environment variables from a ".env" file.
+/// </summary>
+public static class DotEnv
 {
-    public static class DotEnv
+
+    public static void Load()
     {
-        public static void Load()
+        var root = Directory.GetCurrentDirectory();
+        var filePath = Path.Combine(root, ".env");
+        Console.WriteLine(filePath);
+        if (!File.Exists(filePath))
+            return;
+
+        foreach (var line in File.ReadAllLines(filePath))
         {
-            var root = Directory.GetCurrentDirectory();
-            var filePath = Path.Combine(root, ".env");
-            Console.WriteLine(filePath);
-            if (!File.Exists(filePath))
-                return;
+            var parts = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (var line in File.ReadAllLines(filePath))
-            {
-                var parts = line.Split(
-                    '=',
-                    StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+                continue;
 
-                if (parts.Length != 2)
-                    continue;
-
-                Environment.SetEnvironmentVariable(parts[0], parts[1]);
-            }
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
         }
     }
 }
